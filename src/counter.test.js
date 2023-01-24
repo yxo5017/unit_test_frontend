89e5cb1e90b8c527b45aff60962c099fa5d84a23
yxo5017 <yxo5017@icloud.com>
2023-01-24 13:17:59 +0900

    first commit

---
+++ src/counter.test.js
@@ -0,0 +1,34 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import Counter from "./counter";
+
+//test block
+test("increments counter", () => {
+// render the component on virtual dom
+render(<Counter />);
+
+//select the elements you want to interact with
+const counter = screen.getByTestId("counter");
+const incrementBtn = screen.getByTestId("increment");
+
+//interact with those elements
+fireEvent.click(incrementBtn);
+
+//assert the expected result
+expect(counter).toHaveTextContent("1");
+});
+
+
+test("decrease counter", () => {
+    // render the component on virtual dom
+    render(<Counter />);
+    
+    //select the elements you want to interact with
+    const counter = screen.getByTestId("counter");
+    const decrementBtn = screen.getByTestId("decrement");
+    
+    //interact with those elements
+    fireEvent.click(decrementBtn);
+    
+    //assert the expected result
+    expect(counter).toHaveTextContent("-1");
+    });
